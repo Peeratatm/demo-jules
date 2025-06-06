59333268ccdc17baadd98deb834a6a072481b3af
Peeratatm <156899017+Peeratatm@users.noreply.github.com>
2025-06-06 13:15:09 +0700

    feat: Add src/pages/AQADfsoxGxqZGVZ-/index.jsx for new UI component

---
+++ src/pages/AQADfsoxGxqZGVZ-/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SummaryScoreCard from '../../AQADfsoxGxqZGVZ-/components/SummaryScoreCard';
+
+const SummaryPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <SummaryScoreCard />
+    </div>
+  );
+};
+
+export default SummaryPage;
